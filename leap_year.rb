0eaf872075c0af2e996d7435909252bceb8f18cb
zpf0918 <zhanggongzifei@163.com>
2017-07-04 09:38:59 +0800

    add lead_year.rb

--- leap_year.rb
+++ leap_year.rb
@@ -1,11 +1,3 @@
-def is_leap_year?(year)
-  if year % 4 == 0
-    if year % 100 == 0
-      return false
-    else
-      return true
-    end
-  else
-    return false
-  end
+def is_leap_year?(y)
+  y % 400 == 0 || ( y % 4 == 0 && y % 100 !=0 )
 end
